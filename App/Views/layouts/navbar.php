aff2dbb48a8c8b9e79412ec35c02d13dbb11def0
tesafrnta <h1101241057@student.untan.ac.id>
2025-12-02 21:33:43 +0700

    baru

--- App/Views/layouts/navbar.php
+++ App/Views/layouts/navbar.php
@@ -1,13 +1,13 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary mb-4">
     <div class="container">
-        <a class="navbar-brand" href="<?= $base_url ?>/">SIGUDA PPBO</a>
+        <a class="navbar-brand" href="<?= $base_url ?>/dashboard">SIGUDA PPBO</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav me-auto">
                 <li class="nav-item">
-                    <a class="nav-link" href="<?= $base_url ?>/">Dashboard</a>
+                    <a class="nav-link" href="<?= $base_url ?>/dashboard">Dashboard</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="<?= $base_url ?>/kategori">Kategori</a>
@@ -21,7 +21,7 @@
             </ul>
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a class="nav-link btn btn-danger text-white btn-sm px-3" href="<?= $base_url ?>/logout">Logout</a>
+                    <a class="nav-link btn btn-danger text-white btn-sm px-3" href="<?= $base_url ?>/Logout">Logout</a>
                 </li>
             </ul>
         </div>
